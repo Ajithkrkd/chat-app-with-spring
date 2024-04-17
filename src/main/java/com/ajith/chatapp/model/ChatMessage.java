0a49be9f08a78a3215f5c9d67cb7db0cace6cf8d
Ajith krkd <ajith2255iti@gmail.com>
2024-04-17 21:16:22 +0530

    simple chat app

--- src/main/java/com/ajith/chatapp/model/ChatMessage.java
+++ src/main/java/com/ajith/chatapp/model/ChatMessage.java
@@ -1,4 +1,18 @@
 package com.ajith.chatapp.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT, LEAVE, JOIN
+    }
 }
